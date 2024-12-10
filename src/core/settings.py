6ab034162bcf6b5ae165c451dfc759db1d658601
Callum Ilett <cilett99@gmail.com>
2024-12-10 17:53:00 +0000

    🔧 Update Django settings to configure

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,7 +30,15 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://dq3hdmf587.eu-west-2.awsapprunner.com",
+]
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "https://dq3hdmf587.eu-west-2.awsapprunner.com",
+]
 
 
 # Application definition
